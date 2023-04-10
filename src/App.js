19520245080c54e57a43e7775c92d6bbf7ed3060
Aizat I <ibraimova.as@gmail.com>
2023-04-09 21:41:11 -0500

    React hooks, showMore, image carousel

--- src/App.js
+++ src/App.js
@@ -1,24 +1,101 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import { data } from './data';
 
 function App() {
+
+  const [places, setPlaces] = useState(data);
+  const [showText, setShowText] = useState(false);
+  const [images, setImages] = useState(0)
+  const {image} = data[images]
+
+  const removePlace = (id) => {
+    let newPlaces = places.filter(place => place.id !== id)
+    setPlaces(newPlaces)
+  }
+  const showTextClick = (item) => {
+    item.showMore = !item.showMore
+    setShowText(!showText)
+  }
+
+  const previousImage = () => {
+    setImages((images) => {
+      images --;
+      if (images < 0) {
+        images = data.length -1;
+      }
+      return images;
+    })
+  }
+
+  const nextImage = () => {
+    setImages((images => {
+      images ++;
+      if (images > data.length -1) {
+        images = 0;
+      }
+      return images;
+    }))
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className='container'>
+        <h1>{places.length} Top Bucket List Destinations</h1>
+      </div>
+
+      {places.map((item => {
+        const {id, place, description, image, source, showMore} = item;
+  
+        return(
+          <div key={id}>
+            <div className='container'>
+              <h2>{id} - {place}</h2>
+            </div>
+
+            <div className='container'>
+              <p>{showMore ? description : description.substring(0, 220) + "...."}
+              <button onClick={() => showTextClick(item)}>{showMore ? "Show less" : "Show more"}</button></p>
+            </div>
+
+            <div className='container'>
+              <img src={image} width="350px" alt='bucket list'/>
+            </div>
+
+            <div className='container'>
+              <p>{source}</p>
+            </div>
+            <div className='container'>
+              <button onClick={() => removePlace(id)} >Remove</button>
+
+            </div>
+
+          </div>
+        )
+      }))}
+      <div className='container'>
+        <button onClick={() => setPlaces([])}>Remove all</button>
+
+      </div>
+
+      <div className='container'>
+        <h2>Slide of images from the list</h2>
+
+      </div>
+
+      <div className='container'>
+        <img src ={image} width="400px" alt='slide'/>
+      </div>
+      
+      <div className='container btn'>
+        <button onClick={previousImage}>Previous</button>
+        <button onClick={nextImage}>Next</button>
+
+      </div>
+
     </div>
+    
   );
 }
 
---
+++ src/data.js
@@ -0,0 +1,76 @@
+export const data = [
+    {
+        id: 1,
+        place: "Santorini, Greece",
+        description: `You'll be hard-pressed to enjoy a more romantic sunset experience than that found in Santorini. Whether you visit the picturesque capital of Firá or watch the sun descend over the sparkling water in front of beautiful Oia, stepping foot on Santorini is akin to walking into a postcard. This is an ideal honeymoon or bucket list destination for couples.
+
+        The iconic pearly white, blue-domed buildings are made even lovelier by colorful blooms. It's as if an artist was set loose upon each home with only the most tasteful of colors. A bright teal door here and a vibrant yellow wall or red gate there add just the right pop to a perfect natural backdrop.
+        
+        Set upon jagged cliffs jutting out from the most turquoise of seas, Santorini's towns are flanked by narrow, winding streets and terraced homes that butt into each other at all angles. Walking through the area, you may find yourself unwittingly tramping upon someone's roof.`,
+        
+        image: 'https://images.unsplash.com/photo-1571406252241-db0280bd36cd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fHNhbnRvcmluaXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60',
+
+        source: "Booking.com",
+
+        showMore: false
+    },
+
+    {
+        id: 2,
+        place: "Venice, Italy",
+        description:`There's something magical about Venice. Between the intricate canal system, beautiful stone bridges, and harmonious singing of the gondoliers, there's a unique vibe in this Italian city that can't be found elsewhere.
+
+        A boat ride along the aptly named Grand Canal rewards visitors with an intimate look at the highlights of Venetian architecture lining its banks – from historic churches to ornate palaces like the Doge's Palace (a.k.a. Palazzo Ducale), which you'll want to spend at least a couple of hours exploring.
+        
+        Piazza San Marco is one of the main attractions in Venice and serves as a meeting place for locals, but mainly tourists. It's home to the famous and intricate St. Mark's Basilica, but it's so crowded (and filled with pigeons) that you won't get a true glimpse of the city's heart.
+        
+        Perhaps the best way to tour Venice is by getting lost, which is easy to do. Only then will you stumble upon some of the local's favorite haunts, as well as the small, winding alleyways that present so much charm.`,
+        image: "https://images.unsplash.com/photo-1523906921802-b5d2d899e93b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8dmVuaWNlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+        source: "Booking.com",
+        showMore: false
+    },
+
+    {
+        id: 3,
+        place: 'Great Barrier Reef, Australia',
+        description:`Snorkeling Australia's Great Barrier Reef is one of the most incredible experiences you'll ever have. A magical underwater world welcomes adventurers to this incredible locale. Bursting with color and teeming with life, this impressive coral reef spans 2,300 kilometers from the tip of Queensland to the southern town of Bundaberg.
+
+        A World Heritage-listed area, the Great Barrier Reef is home to a rich ecosystem filled with everything from the smallest zooplankton to tropical fish to marine mammals like dugongs and dolphins.
+        
+        A truly peaceful experience, swimming around the magical reef is a gift. Whether you choose to scuba dive or snorkel, you'll experience only the quiet sound of fish nibbling on coral as you roam about this vast marine playground, being constantly amazed by the beauty on display before you.
+        
+        Sadly, global warming has been bleaching (and killing) large sections of this reef (especially in the north), so if you're planning to go, you should book a trip to this popular bucket list travel destination soon. And whatever you do, don't touch the coral.`,
+        image:'https://images.unsplash.com/photo-1582967788606-a171c1080cb0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTN8fGdyZWF0JTIwYmFycmllciUyMHJlZWZ8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
+        source: "Booking.com",
+        showMore: false
+    },
+
+    {
+        id: 4,
+        place: "Tahiti, French Polynesia",
+    
+        description:`Slightly livelier than its laid-back neighbor, Bora Bora, Tahiti offers travelers the same beautiful azure lagoons, lush mountainous landscapes, and lively coral reefs, with a little more excitement thrown in.
+
+        In Papeete, the island's capital, you'll find plenty to help you while away the hours you're not spending on the beach. An impressive mix of French and Polynesian cultures, the city boasts great restaurants, popular resorts, spas, museums, and plenty of places to shop.
+        
+        Outside the city, visitors are greeted by cascading waterfalls, sheltered lagoons, and beautiful valleys. Book a 4WD tour of the island, learn to dive in a lagoon, or board a whale-watching tour boat (between July and October).`,
+        image:'https://images.unsplash.com/photo-1633381685199-99c89e48b8b3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dGFoaXRpfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+        source: "Booking.com",
+        showMore: false
+    },
+
+    {
+        id: 5,
+        place: "Machu Picchu, Peru",
+        description:`It's hard to see a picture of Machu Picchu without wanting to jump right in. The vibrant green grass, majestic craggy peaks, and remarkable stone ruins are breathtakingly beautiful. No wonder this magical spot has been dubbed one of the top places to visit in the world.
+
+        This archeological masterpiece was once an Inca City (in the 15th and 16th centuries). Today, it's one of the most phenomenal of all the UNESCO World Heritage Sites, and one that millions have placed at the top of their bucket lists for good reason.
+        
+        Not surprisingly, Machu Picchu is one of the most visited tourist attractions in Peru. To get here, you'll have to plan your visit well. Entry is only permitted twice a day (in the morning or afternoon), and you'll have to either hike along the famed Inca Trail or take a bus from the nearby town of Aguas Calientes for access.
+        
+        Most visitors stay in historic Cusco, the oldest continuously inhabited city in the Americas, for easier access to the site. The swanky, five-star Palacio Nazarenas, a Belmond Hotel is your best bet for a luxurious stay in Cusco.`,
+        image: 'https://images.unsplash.com/photo-1609944433409-81bda5323abc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8bWFjaHUlMjBwaWNjaHV8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
+        source: "Booking.com",
+        showMore: false
+    }
+]
